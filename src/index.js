b998a5e56bccfa3e26a0e06dddc6b76d47be86c9
Jared Cole <jcole.ny2000@gmail.com>
2020-07-28 14:09:28 -0400

    twilio code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
+import * as tw from './services/twilio-handler';
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.setState({
+      sent: false,
+    });
+  }
+
+  sendMessage = () => {
+    if (!this.state.sent) {
+      tw.sendMessage().then(() => {
+        this.setState({
+          sent: true,
+        });
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <button id="send-message" onClick={this.sendMessage} aria-label="ok" type="button" />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('main'));
