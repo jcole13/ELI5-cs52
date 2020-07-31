c7173c53c321ac3fbbebc29d00792d78a6eea507
Jared Cole <jcole.ny2000@gmail.com>
2020-07-31 12:44:52 -0400

    client-side

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import './style.scss';
 class App extends Component {
   sendMessage = () => {
     console.log('Sending message');
-    fetch('https://damp-plateau-67111.herokuapp.com/send-text?')
+    fetch('https://damp-plateau-67111.herokuapp.com/send-text', {
+      mode: 'no-cors',
+    }).then(() => console.log('sent'))
       .catch((err) => console.error(err));
   }
 
   render() {
     return (
       <div>
-        <button id="send-message" onClick={this.sendMessage} aria-label="ok" type="button" />
+        <h1>Twilio Client Sample</h1>
+        <button id="send-message" onClick={this.sendMessage} aria-label="ok" type="button">
+          <h1>Press me.</h1>
+        </button>
       </div>
     );
   }
